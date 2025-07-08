8ab326d8089fe51e64b7d7668301756b21e714e6
Lokesh2684 <139561396+Lokesh2684@users.noreply.github.com>
2025-07-08 17:55:54 +0530

    Update variables.tf

--- modules/linux_vm/variables.tf
+++ modules/linux_vm/variables.tf
@@ -1,27 +1,19 @@
-variable "resource_group_name" {
-  type = string
+variable "prefix" {
+   type = string
 }
-
 variable "location" {
-  type = string
+   type = string
 }
-
-variable "vm_name" {
-  type = string
+variable "resource_group_name" {
+   type = string
 }
-
-variable "admin_username" {
-  type = string
-}
-
-variable "admin_password" {
-  type = string
-}
-
-variable "ssh_public_key" {
-  type = string
-}
-
 variable "vm_size" {
-  type = string
+   type = string
 }
+variable "admin_username" {
+   type = string
+}
+variable "admin_password" {
+   type = string
+}
+
