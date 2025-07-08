2f1e0a6d2c6c0b6319740b2b50daca3f71c89bc0
Lokesh2684 <139561396+Lokesh2684@users.noreply.github.com>
2025-07-08 17:50:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "Lokesh-RSG-1"        
+    resource_group_name   = "Lokesh-RSG-1"
     storage_account_name  = "lokeshstorageacr"
     container_name        = "tfstate"
     key                   = "prod.terraform.tfstate"
