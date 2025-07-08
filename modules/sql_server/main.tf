7c2dc821047e223de3b349d9544e3bc0136b7f88
Lokesh2684 <139561396+Lokesh2684@users.noreply.github.com>
2025-07-08 17:59:56 +0530

    Update main.tf

--- modules/sql_server/main.tf
+++ modules/sql_server/main.tf
@@ -14,3 +14,7 @@ resource "azurerm_mssql_server" "sql_server" {
 output "name" {
   value = azurerm_mssql_server.sql_server.name
 }
+
+output "sql_server_id" {
+  value = azurerm_mssql_server.sql_server.id
+}
