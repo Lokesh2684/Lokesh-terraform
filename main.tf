07ea74098f21c64dec2b2d28725ddf2d06837dd6
Lokesh2684 <139561396+Lokesh2684@users.noreply.github.com>
2025-07-08 17:50:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,39 +1,28 @@
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
 }
 
-resource "azurerm_resource_group" "main" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
+module "linux_vm" {
+  source              = "./modules/linux_vm"
+  vm_name             = "Loki_assignmnet_vm"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = module.linux_vm_subnet_id
+}
+
 module "sql_server" {
   source              = "./modules/sql_server"
-  resource_group_name = var.resource_group_name
   location            = var.location
-  sql_admin_username  = var.sql_admin_username       
-  sql_admin_password  = var.sql_admin_password
-  server_name         = var.sql_server_name          
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 module "sql_database" {
-  source              = "./modules/sql_database"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = var.location
-  server_name         = module.sql_server.name
-  database_name       = var.sql_database_name
+  source   = "./modules/sql_database"
+  server_id = module.sql_server_id
 }
-
-module "linux_vm" {
-  source              = "./modules/linux_vm"
-  count               = 2
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  vm_name             = "${var.prefix}-vm-${count.index}"
-  admin_username      = var.vm_admin_username       
-  admin_password      = var.vm_admin_password
-  ssh_public_key      = var.ssh_public_key
-  vm_size             = var.vm_size
-}
-
-
